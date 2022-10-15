1241b0c3a2cf6e264858a6e1e7ec43f1ee517889
Mohamed Wael Elkatan <elkatanpython@gmail.com>
2022-10-15 02:55:45 +0200

    Add files via upload
    Transpose matrix

---
+++ Unit 2 : C Programming/LEC_3_C_Basics/Assigment_3/homework3-q1.c
@@ -0,0 +1,68 @@
+/*
+ ============================================================================
+ Name        : homework_3_2.c
+ Author      : Mohammed Elkattan
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int a1[15][15],a2[15][15];
+	int r,c;
+	printf("enter number of rows and columns");
+	fflush(stdin),fflush(stdout);
+	scanf("%d %d",&r ,&c);
+	for(int i=0; i<r;i++){
+		for(int w=0;w<c;w++){
+			printf("enter a1(%d%d):",i+1,w+1);
+			fflush(stdin),fflush(stdout);
+			scanf("%d",&a1[i][w]);
+			printf("\n");
+		}
+	}
+	for(int i=0; i<r;i++){
+		for(int w=0;w<c;w++){
+			printf("enter a2(%d%d):",i+1,w+1);
+			fflush(stdin),fflush(stdout);
+			scanf("%d",&a2[i][w]);
+			printf("\n");
+		}
+	}
+		printf("Matrix a1 is\n");
+		for(int i=0; i<r;i++){
+			for(int w=0;w<c;w++){
+				printf("%d\t",a1[i][w]);
+			}
+			printf("\n");
+		}
+		printf("Matrix a2 is\n");
+		for(int i=0; i<r;i++){
+			for(int w=0;w<c;w++){
+				printf("%d\t",a2[i][w]);
+			}
+			printf("\n");
+		}
+		printf("Transpose matrix  of a2 is\n");
+		float trans[20][20];
+		for(int i=0; i<c;i++){
+					for(int w=0;w<r;w++){
+					trans[i][w]=a2[w][i];
+					}
+					}
+		for(int i=0; i<c;i++){
+					for(int w=0;w<r;w++){
+						printf("%.2f\t",trans[i][w]);
+					}
+
+					printf("\n");
+
+
+	}
+}
+
+
