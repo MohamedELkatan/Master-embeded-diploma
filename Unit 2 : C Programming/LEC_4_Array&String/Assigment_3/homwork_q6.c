0009de8c39248278a23bcc681df0804d3b56a379
Mohamed Wael Elkatan <elkatanpython@gmail.com>
2022-10-15 03:19:54 +0200

    freq of character using array

---
+++ Unit 2 : C Programming/LEC_4_Array&String/Assigment_3/homwork_q6.c
@@ -0,0 +1,34 @@
+/*
+ ============================================================================
+ Name        : homwork_q6.c
+ Author      : Mohammed Elkattan
+ Version     :
+ Copyright   : Your copyright notice
+ Description : freq of character using array in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	while(1){
+		char arrych[700];
+		char a;
+
+		printf("enter a string :");
+		fflush(stdin);fflush(stdout);
+		gets(arrych);
+		printf("enter a character :");
+		fflush(stdin);fflush(stdout);
+		scanf("%c",&a);
+
+		int	counter=0;
+		for(int i=0; arrych[i]!=0;i++){
+
+			if(a==arrych[i]){counter++; }
+		}
+		printf("freq of %c is : %d \n",a,counter);
+	}
+}
+
